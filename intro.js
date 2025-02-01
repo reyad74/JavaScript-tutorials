c88410d8e3539ec9599262e8d135329542e1f09c
Md Reyad <mdreyadh749@gmail.com>
2025-02-01 23:18:22 +0600

    first commit

---
+++ intro.js
@@ -0,0 +1,48 @@
+// let a = 67;
+
+// a ="Hello";
+// console.log(a);
+
+
+
+// nn ss bb  -Primitive data types
+// let a = null;
+// let b = 785;
+// let c = "Hello";
+// let d = false;  
+// let e = undefined;
+// let f = BigInt("1234567890123456789012345678901234567890");
+// let g = Symbol("foo");
+// console.log(a, b, c, d, e, f, g);
+// console.log(typeof a, typeof b, typeof c, typeof d, typeof e, typeof f, typeof g);
+
+//Object(non-primitive) data type - like dictionary in python
+// const items = {
+//     "Riyadh": "Saudi Arabia",
+//     "Dhaka": "Bangladesh",
+//     "love": 143,
+//     "Rakib": "undefined",
+//  }
+// console.log(items);
+// console.log(items["Riyadh"]);
+
+//practice
+let a = 67;
+let b = "Shakib";
+
+console.log(a + b);
+console.log(typeof (a + b));
+
+const info = {
+    name: "Rakib",
+    age: 23,
+    country: "Bangladesh",
+}
+info["city"] = "Dhaka";
+console.log(info.name);
+
+
+console.log(info);
+//5 video end day 1
+
+
